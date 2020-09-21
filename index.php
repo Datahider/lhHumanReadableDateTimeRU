3a3c189b88d8f3483d43ca1cc68e84aaf4971cd0
Πέτρος Ιωαννίδης <drweb_000@user-pc>
2020-09-21 16:42:41 +0300

    Добавлен тест

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <?php
-
+date_default_timezone_set("Europe/Moscow");
 require_once 'lhHumanReadableDateTimeRU/classes/lhHumanReadableDateTimeRU.php';
 
 echo "Hello World!\n";
 
-$dt = new lhHumanReadableDateTimeRU("2020-09-23");
+$dt = new lhHumanReadableDateTimeRU("2020-09-23T09:00:00+03:00");
 echo $dt->when() . "\n";
